a9e475a237ace63715cd52dee7856faa8105b467
leecoading <leehy011011@gmail.com>
2025-01-20 21:22:47 +0900

    [Add] CoadingTest

---
+++ CoadingTest/CoadingTest/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CoadingTest
+{
+    #region 코딩 기초 트레이닝 - 원하는 문자열 찾기(2025.01.20)
+    using System;
+
+    public class Solution
+    {
+        public int solution(string myString, string pat)
+        {
+            myString = myString.ToLower();
+            pat = pat.ToLower();
+
+
+            if (myString.Contains(pat))
+            {
+                return 1;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    #endregion
+}
