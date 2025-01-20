3237489b5e20c24dd846b57660f02f1cfafc813e
leecoading <leehy011011@gmail.com>
2025-01-20 21:55:53 +0900

    [Add]코딩 테스트 연습 - 연습문제 - 크기가 작은 부분 문자열(2025.01.20)

--- CoadingTest/CoadingTest/Program.cs
+++ CoadingTest/CoadingTest/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace CoadingTest
 {
-    #region 코딩 기초 트레이닝 - 원하는 문자열 찾기(2025.01.20)
+    #region 1 코딩 기초 트레이닝 - 원하는 문자열 찾기(2025.01.20)
     using System;
 
     public class Solution
@@ -24,6 +24,30 @@
     }
     #endregion
 
-    #region 코딩 테스트 연습 - 연습문제 - 크기가 작은 부분 문자열(2025.01.20)
+    #region 2 코딩 테스트 연습 - 연습문제 - 크기가 작은 부분 문자열(2025.01.20)
+    public class Solution2
+    {
+        public int solution2(string t, string p)
+        {
+            int answer = 0;
+            int pLength = p.Length;
+            long pInt = long.Parse(p);
+
+            int tLength = t.Length;
+
+            for (int i = 0; i <= tLength - pLength; i++)
+            {
+                //i번째부터 pLength까지 잘라서 sub string에 저장.
+                string sub = t.Substring(i, pLength);
+                //문자열인 sub을 long로 형변환해서 tint에 저장
+                long tInt = long.Parse(sub);
+                if (tInt <= pInt)
+                {
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
     #endregion
 }
